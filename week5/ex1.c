6d2d95c604c439f9f28d0abc05fc73c4a9303d2f
fniyaz <35842362+fniyaz@users.noreply.github.com>
2018-09-19 22:41:21 +0300

    week 5 ex1

---
+++ week5/ex1.c
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define N 100
+
+void *thread(void *arg) {
+  char *ret;
+  printf("Thread # %d\n", arg);
+  
+  pthread_exit(NULL);
+}
+
+main() {
+  pthread_t thid;
+  void *ret;
+
+
+  for(int i = 0; i < N; i++){
+    pthread_create(&thid, NULL, thread, i);
+    printf("Thread # %d created\n", i);
+
+    //pthread_join(thid, &ret);
+  }
+}
