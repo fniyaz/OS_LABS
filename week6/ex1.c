50f5dbae30756c4d8e3696cba20c67684d4383c7
fniyaz <35842362+fniyaz@users.noreply.github.com>
2018-09-26 19:50:20 +0300

    week6 ex1

---
+++ week6/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+	char *a, *b=malloc(13);
+	a = "Hello, Pipe!";
+	int p[2];
+	pipe(p);
+	write(p[1], a, 12);
+	read(p[0], b, 12);
+	printf(b);
+}
